eae432f2db6c2bf121fc367e8ce74d1bde13f532
Dilshad Raza <dilshad-geol@users.noreply.github.com>
2024-03-20 10:46:46 +0500

    Quantitative_Seismic_Interpretation

---
+++ Quantitative_Seismic_Interpretation/README.md
@@ -0,0 +1,8 @@
+# Quantitative Seismic Interpretation
+## Rock Physics via Avseth et al and SEG
+
+This is my adaptation of the methodology described in the book, Quantitative Seismic Interpretation (Avseth et al, 2005).
+
+
+
+Work in progress...
---
+++ Quantitative_Seismic_Interpretation/load_las.py
@@ -0,0 +1,34 @@
+import glob
+import os
+import lasio as lasio
+import numpy as np
+import platform
+
+my_os = platform.system()
+if my_os == 'Windows':
+    path = ".\data"
+elif my_os == 'Linux':
+    path = './data'
+elif my_os == 'Darwin':
+    path = './data'
+
+# get all paths and alphabetically ordered
+paths = sorted(glob.glob(os.path.join(path, "*.las")))
+
+
+def load():
+    well_df = [0] * len(paths)
+
+    for i in range(len(paths)):
+        # read with lasio
+        well = lasio.read(paths[i])
+        # convert to dataframe
+        df = well.df()
+        # in this dataframe, depth is positioned as index, not as column
+        # so better to change depth index to column
+        well_df[i] = df.reset_index()
+        well_df[i].rename(columns={'DEPT': 'DEPTH'}, inplace=True)
+        # replace null values (-9999.0) with NaN
+        well_df[i] = well_df[i].replace(-9999.0, np.NaN)
+
+    return well_df
